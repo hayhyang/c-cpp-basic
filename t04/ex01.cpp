5f9cb00c44dec44d3daa2914c4dee8813d65d415
hay <hayhyang@gmail.com>
2023-06-22 17:10:44 +0900

    feat: 변수란

---
+++ t04/ex01.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int a; // 선언
+
+  a = 3; // 할당
+  printf("%d\n", a);
+
+  a = 5;
+  printf("%d\n", a);
+}
---
+++ t04/ex02.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int a = 2; // 초기화
+  int b = 3;
+
+  printf("%d + %d = %d\n", a, b, a+b);
+  // 변수이름: 알파벳 대소문자, _, 숫자(첫글자 금지)
+}
