a857ec8d153abd082275088c5fad2d27d8597957
hay <hayhyang@gmail.com>
2023-06-22 16:58:35 +0900

    feat: printf로 다양한 데이터 출력하기

---
+++ t03/ex01.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  printf("%d + %d = %d\n", 2,3,5);
+  printf("%f\n", 3.14);
+  printf("%.2f\n", 3.141592);
+  printf("%g\n", 3.141592);
+  printf("%.3g\n", 81239812374.12341234);
+  printf("%c %c %c\n", 'a', 'b', 'c');
+  printf("%s\n", "안녕하세요");
+}
