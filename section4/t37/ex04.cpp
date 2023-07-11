23d948b9173162a7bd5e88b408062fd017fa6bde
hay <hayhyang@gmail.com>
2023-07-11 10:42:08 +0900

    feat: 섹션4 종합문제

---
+++ section4/t37/ex04.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void print_noSpace(char str[]) {
+  for (int i = 0; str[i] != '\0'; i++) {
+    if(str[i] != ' ') printf("%c", str[i]);
+  }
+}
+
+int main() {
+  print_noSpace("Hello, World\n");
+  print_noSpace("My name is Doodle\n");
+}
