8125b0a1df3f2510784350c2912afab48516cff6
hay <hayhyang@gmail.com>
2023-07-18 08:51:11 +0900

    feat: c++ 스타일 입출력

---
+++ section7/t49/ex01.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // 인풋, 아웃풋에 관련된 스트림이 정의되어 있는 파일
+
+using namespace std;
+
+int main() {
+  // std::cout << "Hello, World" << std::endl;
+  // endl end line
+  // std:: namespace, using namespace XXX 를 선언해주면 생략할 수 있다.
+  cout << "Hello world" << endl;
+
+  // << 데이터를 구분해주는 연산자
+}
---
+++ section7/t49/ex03.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int main() {
+  string name;
+  
+  cout << "이름 입력 : " << endl;
+  cin >> name;
+  string message = "안녕하세요, " + name + " 님.";
+  cout << message << endl;
+}
